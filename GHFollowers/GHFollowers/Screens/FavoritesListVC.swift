5559d93943c731f363ce7b0709ac6c7235f972a5
Mikhail Ustyantsev <ustyantsevma@gmail.com>
2024-02-24 07:18:45 +0500

    FavoritesListVC works

---
+++ GHFollowers/GHFollowers/Screens/FavoritesListVC.swift
@@ -0,0 +1,109 @@
+//
+//  FavoritesListVC.swift
+//  GHFollowers
+//
+//  Created by Mikhail Ustyantsev on 21.01.2024.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    let tableView               = UITableView()
+    var favorites: [Follower]   = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureTableView()
+        configureViewController()
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        getFavorites()
+    }
+    
+    
+    private func configureViewController() {
+        view.backgroundColor    = .systemBackground
+        title                   = "Favorites"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    
+    private func configureTableView() {
+        view.addSubview(tableView)
+        tableView.frame         = view.bounds
+        tableView.rowHeight     = 80
+        tableView.delegate      = self
+        tableView.dataSource    = self
+        tableView.register(FavoriteCell.self, forCellReuseIdentifier: FavoriteCell.reuseID)
+    }
+    
+    
+    func getFavorites() {
+        PersistenceManager.retrieveFavorites { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let favorites):
+                
+                if favorites.isEmpty {
+                    self.showEmptyStateView(with: "No Favorites?\nAdd one on the follower screen", in: self.view)
+                } else {
+                    self.favorites = favorites
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                        self.view.bringSubviewToFront(self.tableView)
+                    }
+                }
+            case .failure(let error):
+                self.presentGFAlertOnMainTread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
+    }
+    
+    
+}
+
+
+extension FavoritesListVC: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        favorites.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: FavoriteCell.reuseID, for: indexPath) as? FavoriteCell else { return UITableViewCell() }
+        cell.set(favorite: favorites[indexPath.row])
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let favorite     = favorites[indexPath.row]
+        let destVC       = FollowerListVC()
+        destVC.username  = favorite.login
+        destVC.title     = favorite.login
+        
+        navigationController?.pushViewController(destVC, animated: true)
+    }
+    
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else { return }
+        
+        let favorite        = favorites[indexPath.row]
+        favorites.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .left)
+        
+        PersistenceManager.updateWith(favorite: favorite, actionType: .remove) { [weak self] error in
+            guard let self = self else { return }
+            guard let error = error else { return }
+            self.presentGFAlertOnMainTread(title: "Unable to remove", message: error.rawValue, buttonTitle: "Ok")
+        }
+    }
+    
+}
--- GHFollowers/GHFollowers/Support/SceneDelegate.swift
+++ GHFollowers/GHFollowers/Support/SceneDelegate.swift
@@ -19,37 +19,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = createTabBar()
+        window?.rootViewController = GFTabBarController()
         window?.makeKeyAndVisible()
         
         configureNavigationBar()
     }
-
-    func createSearchNC() -> UINavigationController {
-        let searchVC = SearchViewController()
-        searchVC.title = "Search"
-        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
-        return UINavigationController(rootViewController: searchVC)
-    }
-    
-    func createFavoritesNC() -> UINavigationController {
-        let favoritesListVC = FavoritesListViewController()
-        favoritesListVC.title = "Favorites"
-        favoritesListVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
-        return UINavigationController(rootViewController: favoritesListVC)
-    }
-    
-    func createTabBar() -> UITabBarController {
-        let tabBar = UITabBarController()
-        UITabBar.appearance().tintColor = .systemGreen
-        tabBar.viewControllers = [createSearchNC(), createFavoritesNC()]
-        return tabBar
-    }
-    
     
     func configureNavigationBar() {
         UINavigationBar.appearance().tintColor = .systemGreen
     }
+    
+    
+ 
     
     
     func sceneDidDisconnect(_ scene: UIScene) {
